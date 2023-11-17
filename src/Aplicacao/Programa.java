a81efc513bee4626c60eddb797bf68b79c97f704
vinicarmo <vinicius.carmo1106@gmail.com>
2023-11-17 15:39:38 -0300

    implementando tabuleiro e peças

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,10 @@
 package Aplicacao;
 
-import Tabuleiro.Posicao;
+import JogoDeTabuleiro.Posicao;
+import JogoDeTabuleiro.Tabuleiro;
 
 public class Programa {
     public static void main(String[] args) {
-        Posicao posicao = new Posicao(3, 5);
-        System.out.println(posicao);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
 }
--- src/JogoDeTabuleiro/Posicao.java
+++ src/JogoDeTabuleiro/Posicao.java
@@ -1,4 +1,4 @@
-package Tabuleiro;
+package JogoDeTabuleiro;
 
 public class Posicao {
     private int coluna;
